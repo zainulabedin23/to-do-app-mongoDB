c24b7896c23bbb5c6459c3fcd6dc8d9970945875
zainulabedin23 <abedinz427@gmail.com>
2024-05-29 13:03:33 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({
 
-    origin:["https://to-do-app-mongo-db-sooty.vercel.app"],
+    origin:["https://to-do-app-mongo-db-black.vercel.app"],
       methods:['POST','GET'],
       credentials: true
     }));
