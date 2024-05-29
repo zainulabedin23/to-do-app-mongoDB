9a736700a8c9df670733bca98a41a41b126ef48b
zainulabedin23 <abedinz427@gmail.com>
2024-05-29 12:28:28 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:["https://to-do-app-six-dun.vercel.app/"],
+      methods:['POST','GET'],
+      credentials: true
+    }));
+    
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
